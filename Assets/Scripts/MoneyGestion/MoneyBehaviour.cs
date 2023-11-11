e9f9c2c65c80a71a038380929c43dfd597363576
Maximilien <40921999+Maxodie@users.noreply.github.com>
2023-11-11 18:45:40 +0100

    feat : player, MoneyBehaviour

--- Assets/Scripts/MoneyGestion/MoneyBehaviour.cs
+++ Assets/Scripts/MoneyGestion/MoneyBehaviour.cs
@@ -1,15 +1,15 @@
 public static class MoneyBehaviour {
-    /*public static void UpdateMoney(Player player, int amount) {
-        Player.coins += amount;
-        if(Player.coins < 0)
+    public static void UpdateMoney(Player player, int amount) {
+        player.coins += amount;
+        if(player.coins < 0)
             player.coins = 0;
-    }*/
+    }
 
-    /*public static void PlayerTransaction(Player receiver, Player giver, int amount) {
-        UpdateMoney
-    
-        Player.coins += amount;
-        if(Player.coins < 0)
-            player.coins = 0;
-    }*/
+    public static void PlayerTransaction(Player receiver, Player giver, int amount) {
+        UpdateMoney(receiver, amount);
+        UpdateMoney(giver, -amount);
+
+        if(giver.coins < 0)
+            giver.coins = 0;
+    }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,3 @@
-public class Player
-{
-    
+public class Player {
+    public int coins;
 }
