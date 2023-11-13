5e9ffeafdcf1d4aa37a3b4e68f929c7788ae19a6
La Loutre Cosmique <sushi.laque77@gmail.com>
2023-11-13 10:48:15 +0100

    New monument attribute

--- Assets/Scripts/Cards/Monument/Port.cs
+++ Assets/Scripts/Cards/Monument/Port.cs
@@ -1,8 +1,8 @@
 public class Port : Monument
 {
     public Port(string cardName, CardType cardType, string cardEffectDescription, int constructionCost, int gains,
-        CardType requiredCardType)
-        : base(cardName, cardType, cardEffectDescription, constructionCost, gains, requiredCardType)
+        CardType requiredCardType, bool built)
+        : base(cardName, cardType, cardEffectDescription, constructionCost, gains, requiredCardType, built)
     {
         
     }
