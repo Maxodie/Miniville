804f4fa3a6f0101b4e0cdc8ea7e646383c01f093
Porolito <pierre.thomas16380@gmail.com>
2023-11-13 10:53:20 +0100

    fix: change type of buildingCard and monumentCards

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,10 +6,10 @@ public class Player
     public int coins;
     public int maxDices; //add
     public int currentDice; //add
-    public List<Card> buildingCards = new List<Card>();
-    public Card[] monumentCards = new Card[4];
+    public List<Establishment> buildingCards = new List<Establishment>();
+    public Monument[] monumentCards = new Monument[4];
     
-    public Player(string name, int coins, int maxDices, int currentDice, List<Card> deck, Card[] monument) //add
+    public Player(string name, int coins, int maxDices, int currentDice, List<Establishment> deck, Monument[] monument) //add
     {
         this.name = name;
         this.coins = coins;
@@ -19,18 +19,18 @@ public class Player
         this.monumentCards = monument;
     }
     
-    public void AddCard(Card card) //Méthode à rajouter dans l'UML //The method add a card to the player deck
+    public void AddCard(Establishment establishment) //Méthode à rajouter dans l'UML //The method add a card to the player deck
     {
-        buildingCards.Add(card);
+        buildingCards.Add(establishment);
     }
     
     public int GetCardsNbByType(CardType cardType) //Since some cards have effects depending on what number of that precise type of card you have, we need a function that count a specific type of card
     {
         int count = 0;
         
-        foreach (var card in buildingCards) //We compare all the card type that the player have with the card type we want to check
+        foreach (var establishment in buildingCards) //We compare all the card type that the player have with the card type we want to check
         {
-            if (card.cardType == cardType)
+            if (establishment.cardType == cardType)
             {
                 count++;
             }
