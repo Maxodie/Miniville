529afae3dd608a7a105dbf868415c5e5984cb7b1
La Loutre Cosmique <sushi.laque77@gmail.com>
2023-11-14 12:46:22 +0100

    feat: Monuments cards database
    Updated Game Database to set Monument cards from cards.json

---
+++ Assets/Scripts/Cards/CardHolders/MonumentHolder.cs
@@ -0,0 +1,11 @@
+using System;
+
+[Serializable]
+public class MonumentHolder
+{
+    public string cardName;
+    public string cardEffectDescription;
+    public int constructionCost;
+    public int gains;
+    public int requiredCardTypeID;
+}
