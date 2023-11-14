977056fdef6adb698496c09c6ec407d712ba10af
notpatern <132772266+notpatern@users.noreply.github.com>
2023-11-14 16:33:28 +0100

    feat: transactionbehavior

--- Assets/Scripts/GameStates/TurnStates/InteractionBehaviour.cs
+++ Assets/Scripts/GameStates/TurnStates/InteractionBehaviour.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
-public class InteractionBehaviour : ITurnState {
+public class InteractionBehaviour : ITurnState 
+{
     GameData gameData;
     int playerTurn;
     TurnState turnState;
 
+    [Header("TransactionUi")]
+    [SerializeField] private GameObject uiPanel;
+    [SerializeField] 
+    [SerializeField] private Button playerChoiceButton;
+    private int selectedPlayer;
+    
+    
     [SerializeField] GameObject turnInfoPanel; 
     public void InitState(GameData gameData, int playerTurn, TurnState turnState) {
         this.gameData = gameData;
@@ -14,56 +22,29 @@ public class InteractionBehaviour : ITurnState {
         this.turnState = turnState;
     }
 
-    public void Start() {
-        PayTransaction();
+    public void Start()
+    {
+        turnInfoPanel.SetActive(true);
     }
 
-    public void Update(float dt) {
+    public void Update(float dt)
+    {
 
     }
 
-    public void QuitState() {
+    public void QuitState()
+    {
+        turnInfoPanel.SetActive(false);
+    }
+    
+
+    void SelectPlayer()
+    {
         
     }
 
-    void PayTransaction() {
-        Player currentPlayer;
-
-        //Check red cards on other players 
-        for(int i=gameData.players.Count-1; i >= 0; i--) {
-            currentPlayer = gameData.players[i];
-
-            //don't active currentPlayer red cards
-            if(i != playerTurn) {
-                MoneyTransaction(CardPriority.FIRSt, currentPlayer);
-            }
-        }
-
-        PlayerPaid();
-    }
-
-    void MoneyTransaction(CardPriority cardPriority, Player currentPlayer) {
-        Establishment currentEstablishment;
-
-        for(int i=0; i < currentPlayer.buildingCards.Count; i++) {
-            currentEstablishment = currentPlayer.buildingCards[i];
-
-            if(currentEstablishment.cardPriority == cardPriority)
-                currentEstablishment.PerformSpecial(currentPlayer, gameData.players[playerTurn]);
-        }
-
-        ConfirmSelection();
-    }
-
-    void PlayerPaid() {
-        MoneyTransaction(CardPriority.SECOND, gameData.players[playerTurn]);
-    }
-
-    void SelectPlayer() {
-
-    }
-
-    void ConfirmSelection() {
+    void ConfirmSelection()
+    {
         turnState.Build();
     }
 }
