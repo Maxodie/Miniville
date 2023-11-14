ac3899d10350eca19e389d4df4645dbf75ea6796
La Loutre Cosmique <sushi.laque77@gmail.com>
2023-11-14 14:24:49 +0100

    feat: Added card database for Establishments
    Same as monuments, but cards.json update is missing

---
+++ Assets/Scripts/Cards/CardHolders/EstablishmentHolder.cs
@@ -0,0 +1,13 @@
+using System;
+
+[Serializable]
+public class EstablishmentHolder
+{
+    public string cardName;
+    public string cardColor;
+    public int cardTypeID;
+    public string cardEffectDescription;
+    public int[] constructionCost;
+    public int gains;
+    public int requiredCardTypeID;
+}
--- Assets/Scripts/GameStates/GameData.cs
+++ Assets/Scripts/GameStates/GameData.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class GameData {
     public List<Player> players = new List<Player>();
     public List<Monument> monuments = new List<Monument>();
+    public Dictionary<Establishment, int> establishments = new Dictionary<Establishment, int>();
     
     const string jsonPath = "cards";
 
@@ -16,55 +17,108 @@ public class GameData {
     }
     
     /// <summary>
-    /// Get every cards to json file and put on cards list
+    /// Fill monuments List and establishments Dictionary
     /// </summary>
     void InitDatabase()
     {
         // Get json data
-        TextAsset monumentsData = Resources.Load<TextAsset>(jsonPath);
-        // Store monument holders data
-        CardHolder holders = JsonUtility.FromJson<CardHolder>(monumentsData.text);
+        TextAsset jsonData = Resources.Load<TextAsset>(jsonPath);
+        // Convert json text to CardHolder
+        CardsHolder cardsHolder = JsonUtility.FromJson<CardsHolder>(jsonData.text);
         
+        SetMonumentList(cardsHolder.monumentHolders);
+        SetEstablishmentDictionary(cardsHolder.establishmentHolders);
+    }
+
+    /// <summary>
+    /// Fill Monuments List
+    /// </summary>
+    /// <param name="monumentHolders">Array of monuments card</param>
+    private void SetMonumentList(MonumentHolder[] monumentHolders)
+    {
         // Create instance of each holder type and add it to monumentsList
         Type monumentType = null;
-        for (int i = 0; i < holders.monumentHolders.Length; i++)
+        for (int i = 0; i < monumentHolders.Length; i++)
         {
-            MonumentHolder holder = holders.monumentHolders[i];
+            MonumentHolder card = monumentHolders[i];
             switch (i)
             {
-                case 0:
-                    monumentType = typeof(TrainStation);
+                case 0: monumentType = typeof(TrainStation);
                     break;
-                case 1:
-                    monumentType = typeof(ShoppingMall);
+                case 1: monumentType = typeof(ShoppingMall);
                     break;
-                case 2:
-                    monumentType = typeof(RadioTower);
+                case 2: monumentType = typeof(RadioTower);
                     break;
-                case 3:
-                    monumentType = typeof(AmusementPark);
+                case 3: monumentType = typeof(AmusementPark);
                     break;
             }
             
+            // Create instance of each card with right Type and add it to monuments List
             monuments.Add((Monument)Activator.CreateInstance(monumentType, 
-                holder.cardName,
+                card.cardName,
                 CardType.CITYLIFE,
-                holder.cardEffectDescription,
-                holder.constructionCost,
-                holder.gains,
-                (CardType)holder.requiredCardTypeID,
+                card.cardEffectDescription,
+                card.constructionCost,
+                card.gains,
+                (CardType)card.requiredCardTypeID,
                 false));
         }
-        
-        Debug.Log(monuments[0]);
-        Debug.Log(monuments[0].requiredCardType);
-        Debug.Log(monuments[1]);
-        Debug.Log(monuments[1].requiredCardType);
-        Debug.Log(holders.monumentHolders.Length);
+    }
+
+    /// <summary>
+    /// Fill establishments Dictionary
+    /// </summary>
+    /// <param name="establishmentHolders">Array of establishments card</param>
+    private void SetEstablishmentDictionary(EstablishmentHolder[] establishmentHolders)
+    {
+        // Get color of cards
+        Type establishmentType = null;
+        int nbCard = 6;
+        foreach (EstablishmentHolder card in establishmentHolders)
+        {
+            switch (card.cardColor.ToUpper())
+            {
+                case "BLUE": establishmentType = typeof(BlueCard);
+                    break;
+                case "GREEN": establishmentType = typeof(GreenCard);
+                    break;
+                case "RED": establishmentType = typeof(RedCard);
+                    break;
+                case "PURPLE":
+                    // Each purple cards are specials and can't be stored as PurpleCard
+                    // Set directly cardType by card name who inherit PurpleCard
+                    switch (card.cardName.ToUpper())
+                    {
+                        case "STADIUM": 
+                            establishmentType = typeof(Stadium);
+                            nbCard = 4;
+                            break;
+                        case "BUSINESS CENTER": 
+                            establishmentType = typeof(BusinessCenter);
+                            nbCard = 4;
+                            break;
+                        case "TV STATION": 
+                            establishmentType = typeof(TvStation);
+                            nbCard = 4;
+                            break;
+                    }
+                    break;
+            }
+            
+            // Create instance of each card with right Type and add it to establishment Dictionary
+            establishments.Add((Establishment)Activator.CreateInstance(establishmentType, 
+                card.cardName,
+                (CardType)card.cardTypeID,
+                card.cardEffectDescription,
+                card.constructionCost,
+                card.gains,
+                (CardType)card.requiredCardTypeID), nbCard);
+        }
     }
 }
 
-public class CardHolder
+public class CardsHolder
 {
     public MonumentHolder[] monumentHolders;
+    public EstablishmentHolder[] establishmentHolders;
 }
