afee12336328378b71fa6bb05c9b0ffc482186d8
La Loutre Cosmique <sushi.laque77@gmail.com>
2023-12-15 11:52:00 +0100

    feat: comments (suite)

--- Assets/Scripts/Cards/CardGoPrefab.cs
+++ Assets/Scripts/Cards/CardGoPrefab.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
+// ScriptableObject defining the visual elements for a card in the game
 [CreateAssetMenu(fileName ="CardGo", menuName ="Minivilles/CardGo")]
 public class CardGoPrefab : ScriptableObject {
-    public string cardName;
-    public GameObject cardGo;
-    public GameObject buildingGo;
-
+    public string cardName; // Name of the card prefab
+    public GameObject cardGo; // Visual representation prefab for the card
+    public GameObject buildingGo; // Visual representation prefab for the associated building
 }
--- Assets/Scripts/Cards/CardUIPrefab.cs
+++ Assets/Scripts/Cards/CardUIPrefab.cs
@@ -6,17 +6,28 @@ using TMPro;
 using UnityEngine.EventSystems;
 
 public class CardUIPrefab {
+    // GameObject for the card UI
     public GameObject loadedGo;
+    // Button component of the card UI
     [HideInInspector] public Button loadedBtn;
+    // Sprite for the card
     public Sprite cardSprite;
+    // Image for the card type icon
     public Image iconType;
+    // Text for the card name
     public TMP_Text cardName;
+    // Text for the card description
     public TMP_Text cardDescription;
+    // EventTrigger for handling hover events
     EventTrigger hoverEventCo;
+    // Reference to MonoBehaviour
     MonoBehaviour monoBehaviour;
+    // Reference to BuildBehaviour (optional)
     BuildBehaviour buildBehaviour;
+    // Reference to InteractionBehaviour (optional)
     InteractionBehaviour interactionBehaviour;
     
+    // Constructor to initialize the CardUIPrefab
     public CardUIPrefab(CardUIData cardUIData, Transform spawnPoint, Card card, MonoBehaviour monoBehaviour, BuildBehaviour buildBehaviour = null, InteractionBehaviour interactionBehaviour = null) {
         loadedGo = Object.Instantiate(cardUIData.objectPrefab, spawnPoint);
         this.monoBehaviour = monoBehaviour;
@@ -26,32 +37,39 @@ public class CardUIPrefab {
         LoadGoCard(cardUIData, card);
     }
 
+    // Method to load the visual elements of the card UI
     public void LoadGoCard(CardUIData cardUIData, Card card) {
         loadedBtn = loadedGo.GetComponent<Button>();
         loadedBtn.image.sprite = card.cardSprite;
         loadedBtn.onClick.AddListener(() => monoBehaviour.StopAllCoroutines());
-        
-         for(int i=0; i <loadedGo.transform.childCount; i++) {
+
+        // Iterate through children of the loaded GameObject and assign sprites/text based on their tags
+        for (int i = 0; i < loadedGo.transform.childCount; i++) {
             Transform child = loadedGo.transform.GetChild(i);
 
-            if(cardUIData.cardImgStringTag == child.tag)
+            // Set card image
+            if (cardUIData.cardImgStringTag == child.tag)
                 child.GetComponent<Image>().sprite = card.cardSprite;
 
-            if(cardUIData.iconTypeStringTag == child.tag)
+            // Set icon type image
+            if (cardUIData.iconTypeStringTag == child.tag)
                 child.GetComponent<Image>().sprite = card.cardTypeIcon;
-            
-            if(cardUIData.cardNameTextStringTag == child.tag)
+
+            // Set card name text
+            if (cardUIData.cardNameTextStringTag == child.tag)
                 child.GetComponent<TMP_Text>().text = card.cardName;
 
-            if(cardUIData.cardDescriptionStringTag == child.tag)
+            // Set card description text
+            if (cardUIData.cardDescriptionStringTag == child.tag)
                 child.GetComponent<TMP_Text>().text = card.cardEffectDescription;
 
-            if(cardUIData.costCardTag == child.tag)
+            // Set card cost text
+            if (cardUIData.costCardTag == child.tag)
                 child.GetComponent<TMP_Text>().text = card.constructionCost.ToString();
         }
 
         hoverEventCo = loadedGo.GetComponent<EventTrigger>();
-         
+
         // Set on pointer enter event listener
         EventTrigger.Entry hoverEvent = new EventTrigger.Entry()
         {
@@ -67,29 +85,31 @@ public class CardUIPrefab {
         };
         hoverExitEvent.callback.AddListener(StopDisplayCardRoutine);
         hoverEventCo.triggers.Add(hoverExitEvent);
-        
     }
 
+    // Method to destroy the loaded GameObject
     public void Destroy() {
         Object.Destroy(loadedGo);
     }
 
-    void StartDisplayCardRoutine(BaseEventData eventData)
-    {
+    // Method to start displaying card information on hover
+    void StartDisplayCardRoutine(BaseEventData eventData) {
         monoBehaviour.StartCoroutine(DisplayCard());
     }
 
-    void StopDisplayCardRoutine(BaseEventData eventData)
-    {
+    // Method to stop displaying card information on hover exit
+    void StopDisplayCardRoutine(BaseEventData eventData) {
         monoBehaviour.StopAllCoroutines();
-        
+
+        // Hide up-sized card when hovering stops
         buildBehaviour?.DisplayUpSizedCard(false);
         interactionBehaviour?.DisplayUpSizedCard(false);
     }
 
-    IEnumerator DisplayCard()
-    {
+    // Coroutine to display card information after a delay
+    IEnumerator DisplayCard() {
         yield return new WaitForSeconds(1);
+        // Show up-sized card with the card sprite
         buildBehaviour?.DisplayUpSizedCard(true, cardSprite);
         interactionBehaviour?.DisplayUpSizedCard(true, cardSprite);
     }
