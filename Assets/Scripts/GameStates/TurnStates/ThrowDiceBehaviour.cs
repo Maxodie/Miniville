7b8d4869ca23b090316e33d398291afdc0d7f41d
ItsNicow <its.nicow@gmail.com>
2023-12-15 16:06:00 +0100

    Revert "Merge branch 'Nicow'"
    This reverts commit 026d643f34b31c9469749f82ec384a744d133853, reversing
    changes made to c8ab01c020939648c80c9e512af467880810964d.

--- Assets/Scripts/GameStates/TurnStates/ThrowDiceBehaviour.cs
+++ Assets/Scripts/GameStates/TurnStates/ThrowDiceBehaviour.cs
@@ -1,67 +1,107 @@
-using System;
 using UnityEngine.UI;
 using UnityEngine;
 
+// Serializable class implementing behavior for throwing dice during a turn state
 [System.Serializable]
 public class ThrowDiceBehaviour : ITurnState {
+    bool isBtnInit = false;
+    bool isThrowDiceRestarted = false;
     GameData gameData;
     int playerTurn;
     TurnState turnState;
+    UIData uiData;
     [SerializeField] Button throwOneDice;
     [SerializeField] Button throwTwoDice;
 
     [SerializeField] GameObject playerDicePanel;
+    [SerializeField] PlayerDice playerDice;
 
-    public void InitState(GameData gameData, int playerTurn, TurnState turnState) {
+    [Header("Restart ThrowDice")]
+    [SerializeField] GameObject restartPanel;
+    [SerializeField] Button yesRestartBtn;
+    [SerializeField] Button noRestartBtn;
+
+    // Method to initialize the state with necessary data
+    public void InitState(GameData gameData, int playerTurn, TurnState turnState, UIData uiData) {
         this.gameData = gameData;
         this.playerTurn = playerTurn;
         this.turnState = turnState;
+        this.uiData = uiData;
+
+        restartPanel.SetActive(false);
 
         InitButtons();
-        playerDicePanel.SetActive(false);
 
         Start();
     }
 
+    // Method invoked at the start of the throw dice phase
     public void Start() {
         playerDicePanel.SetActive(true);
 
-        if(gameData.players[playerTurn].maxDice > 1)
+        throwOneDice.interactable = true;
+        if (gameData.players[playerTurn].currentDice > 1) {
             throwTwoDice.interactable = true;
-        else
+            throwTwoDice.GetComponent<Image>().sprite = uiData.blueBtnSprite;
+        } else {
             throwTwoDice.interactable = false;
+            throwTwoDice.GetComponent<Image>().sprite = uiData.grayBtnSprite;
+        }
+
+        gameData.players[playerTurn].OptionalPlayerThrowDice(this, gameData);
     }
     
+    // Method to initialize buttons' click listeners
     void InitButtons() {
+        if (isBtnInit) return;
+
         throwOneDice.onClick.AddListener(PlayerThrowOneDice);
         throwTwoDice.onClick.AddListener(PlayerThrowTwoDice);
+
+        yesRestartBtn.onClick.AddListener(YesRestartThrowDice);
+        noRestartBtn.onClick.AddListener(QuitState);
+
+        isBtnInit = true;
     }
 
+    // Method for update operations during the throw dice phase
     public void Update(float dt) {
-
+        // Perform necessary update operations if any
     }
 
-<<<<<<< Updated upstream
+    // Method to exit the throw dice state
     public void QuitState() {
         playerDicePanel.SetActive(false);
-=======
-    // Method to exit the throw dice state
-    public void QuitState() {        
+        
         isThrowDiceRestarted = false;
->>>>>>> Stashed changes
         turnState.Transactions();
     }
 
-    public void PlayerThrowOneDice() {
-        gameData.players[playerTurn].ThrowDice(1);
-        QuitState();
+    // Method to handle the end of dice throwing
+    void EndThrow() {
+        if (!isThrowDiceRestarted && gameData.players[playerTurn].GetMonumentBuiltByType(typeof(RadioTower))) {
+            restartPanel.SetActive(true);
+        } else {
+            QuitState();
+        }
     }
 
+    // Method to restart the throw dice phase
+    void YesRestartThrowDice() {
+        playerDicePanel.SetActive(false);
+
+        isThrowDiceRestarted = true;
+        turnState.ThrowDice();
+    }
+
+    // Method for a player to throw one dice
+    public void PlayerThrowOneDice() {
+        playerDice.ThrowDice(1, gameData.players[playerTurn].ThrowDice, EndThrow);
+        DisableBtn();
+    }
+
+    // Method for a player to throw two dice
     public void PlayerThrowTwoDice() {
-<<<<<<< Updated upstream
-        gameData.players[playerTurn].ThrowDice(2);
-        QuitState();
-=======
         playerDice.ThrowDice(2, gameData.players[playerTurn].ThrowDice, EndThrow);
         DisableBtn();
     }
@@ -70,8 +110,5 @@ public class ThrowDiceBehaviour : ITurnState {
     void DisableBtn() {
         throwOneDice.interactable = false;
         throwTwoDice.interactable = false;
-
-        playerDicePanel.SetActive(false);
->>>>>>> Stashed changes
     }
 }
